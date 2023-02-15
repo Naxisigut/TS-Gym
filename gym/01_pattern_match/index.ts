4073d41d1e40944db146502e53e577796fbec689
“lcc” <chengcheng.li@51hgp.com>
2023-02-15 20:53:12 +0800

    feat: 重构变换

--- gym/01_pattern_match/index.ts
+++ gym/01_pattern_match/index.ts
@@ -3,7 +3,12 @@ type PromiseValue<T> = T extends Promise<infer value> ? value : never
 type p = Promise<111>
 type res01 = PromiseValue<p>
 
-/* ---------数组--------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* -----------------------------------数组-------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+
 /* 提取首元素类型 */
 type getArrFirst<T> = T extends [infer first, ...unknown[]] ? first : never
 type res02 = getArrFirst<[1, 2, 3]>
@@ -28,8 +33,12 @@ type res11 = shiftArrFirst<[1, 2, 3]>
 type res12 = shiftArrFirst<[]>
 type res13 = shiftArrFirst<1>
 
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ---------------------------------字符串-------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
 
-/* ---------字符串--------- */
 /* 判断是否以某字符串开头 */
 type isStartWith<T extends string, Pre extends string> = T extends `${Pre}${string}` ? true : false
 type res14 = isStartWith<'123', '12'>
@@ -62,8 +71,12 @@ type res23 = trim<'    t    '>
 type res24 = trim<'  '>
 type res25 = trim<''>
 
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ---------------------------------函数---------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
 
-/* ---------函数--------- */
 /* 提取函数的参数数组类型 */
 type getParams<T extends Function> = T extends (...args: infer args) => any ? args : never
 type res26 = getParams<(a: string, b: number, c: undefined)=>boolean>
@@ -91,25 +104,76 @@ type res31 = getThis<()=>void>
 // type res32 = getThis<testThisClass.hello> // 不能这样写，因为这里传入的是一个函数，而不是一个函数类型
 type res32 = getThis<typeof testThisClass.hello> 
 
-/* ---------构造器--------- */
+
+
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* -------------------------------构造器---------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+
 /* 构造器本质上就是一个函数，但是通常用来创建对象，一般使用new语法。 */
 /* 构造器可以使用interface声明 */
 interface man {
     name: string
 }
 interface newMan {
-    new(name: string): man
+    new(name: string): man // 构造器
 }
-/*  */
-type getInstance<T extends new (...args: any) => any>
+/* 获取构造器实例类型 */
+type getConstructorInstance<T>
     = T extends new (...args: any) => infer instance // 构造器仅仅是在普通函数的前面增加了new
     ? instance
     : undefined
-type res33 = getInstance<newMan>
-// type res34 = getInstance<()=>boolean> // 虽然在js中，任何函数都可以使用new语法，但在ts中普通函数是无法满足约束extends new的
+type res33 = getConstructorInstance<newMan>
+type res34 = getConstructorInstance<()=>boolean> // 虽然在js中，任何函数都可以使用new语法，但在ts中普通函数是无法满足约束extends new的
 
 /* 构造器ps */
 // function testConstructor(){
 //     return false
 // }
 // const r = new testConstructor() //这样的代码在js中是合法的，r为一个空对象
+
+/* 获取构造器参数类型 */
+type getConstructorParams<T> = T extends new (...args: infer params)=>any ? params : never
+type res35 = getConstructorParams<newMan>
+type res36 = getConstructorParams<()=>boolean>
+
+
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* -------------------------------索引类型-------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+
+interface indexType{
+    name: string,
+    age: number,
+    isHealthy: boolean
+}
+interface indexType2{
+    age: number,
+    isHealthy: boolean
+}
+/* 获取索引类型所有的key的字面量组成的联合类型 */
+type getKeysType<T> = keyof T
+type res37 = getKeysType<indexType> // 'name' | 'age' | 'isHealthy'  
+
+/* 获取索引类型所有的key的类型组成的联合类型 */
+/**
+ * 说明：
+ * 联合类型具有分布式的特点，所以 indexType['name' | 'age' | 'isHealthy']
+ * = indexType['name'] | indexType['age'] | indexType['isHealthy']
+ * = string | number | boolean
+ */
+type getValuesType<T> = T[keyof T]
+type res38 = getValuesType<indexType>
+
+/* 获取索引类型某个key的值的类型 */
+type getNameType<T extends object> = 'name' extends keyof T ? T['name'] : never
+type getAgeType<T> = T extends { age?: infer ageType | undefined } ? ageType : never
+type res39 = getNameType<indexType>
+type res40 = getNameType<indexType2>
+type res41 = getNameType<{ age?: number }>
+type res42 = getAgeType<indexType>
+type res43 = getAgeType<{ name: string}>
