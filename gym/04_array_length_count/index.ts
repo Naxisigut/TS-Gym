dc907925aed65f5be6a768c8490385a19e463481
“lcc” <chengcheng.li@51hgp.com>
2023-03-01 21:01:33 +0800

    store: 2023.03.01

--- gym/04_array_length_count/index.ts
+++ gym/04_array_length_count/index.ts
@@ -4,3 +4,61 @@ type res093 = add<2, 4>
 
 /* 减 */
 type substract<num1 extends number, num2 extends number> = 
+  createArr<num1> extends [...createArr<num2>, ...infer rest] ? rest['length'] : never
+type res094 = substract<5, 1>
+
+/* 乘 */
+type multiply<num1 extends number, num2 extends number, result extends unknown[] = []> = 
+  num1 extends 0 ? result['length'] : multiply<substract<num1, 1>, num2, [...createArr<num2>, ...result]>
+type res095 = multiply<3, 5>
+
+/* 除 */
+type divide<num1 extends number, num2 extends number, result extends unknown[] = []> = 
+  num1 extends 0 ? result['length'] : divide<substract<num1, num2>, num2, [...result, unknown]>
+type res096 = divide<15, 5>
+
+/* 求字符串长度 */
+type getStrLength<str extends string, result extends unknown[] = []> = 
+  str extends '' 
+  ? result['length'] 
+  : str extends `${string}${infer rest}`
+   ? getStrLength<rest, [...result, unknown]>
+   : never
+type getStrLength_2<str extends string, result extends unknown[] = []> = 
+  str extends `${string}${infer rest}`
+  ? getStrLength<rest, [...result, unknown]>
+  : result['length']
+type res097 = getStrLength<'ffff'>
+type res098 = getStrLength<''>
+type res099 = getStrLength<'fff'>
+type res100 = getStrLength<''>
+
+/* 两数比较 */
+type isBigger<num1 extends number, num2 extends number, countArr extends unknown[] = []> =
+  num1 extends num2 
+  ? false // 相等
+  : countArr['length'] extends num1
+    ? false // num1较小
+    : countArr['length'] extends num2
+      ? true // num1较大
+      : isBigger<num1, num2, [...countArr, unknown]>
+type res101 = isBigger<3, 6>
+type res102 = isBigger<3, 2>
+type res103 = isBigger<3, 3>
+
+/* 获取斐波那契的第n项 */
+type fibonacciLoop<preArr extends unknown[], currArr extends unknown[], indexArr extends unknown[] = [], num extends number = 1> =
+  indexArr['length'] extends num
+  ? currArr['length']
+  : fibonacciLoop<currArr, [...preArr, ...currArr], [...indexArr, unknown], num>
+type getFibonacciN<n extends number> = fibonacciLoop<[1], [], [], n>
+type res104 = getFibonacciN<8>
+type res105 = getFibonacciN<0>
+
+// <[1], [], []>
+// <[], [1], [unknown]>
+// <[1], [1], [unknown, unknown]>
+// <[1], [1, 1], [unknown, unknown, unknown]>
+// <[1, 1], [1, 1, 1], [unknown, unknown, unknown, unknown]>
+// <[1, 1, 1], [1, 1, 1, 1, 1], [unknown, unknown, unknown, unknown, unknown]>
+// <[1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1], [unknown, unknown, unknown, unknown, unknown, unknown]>
---
+++ gym/05_simplify_union/index.ts
@@ -0,0 +1,56 @@
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* --------------------------------联合类型-------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------- */
+/* 单独大写联合类型中的a */
+type upperCaseA<T extends string> = T extends 'a' ? Uppercase<T> : T
+type res106 = upperCaseA<'a' | 'b' | 'c'>
+
+/* 判断联合类型 */
+/* 只有？左边的类型变量会触发分布式 */
+/* 并且[T] [P]的写法并不会触发分布式 */
+type isUnion<T, P = T> = 
+  T extends T 
+  ? [P] extends [T]
+    ? false
+    : true
+  : never
+type res107 = isUnion<1 | 2>
+type res108 = isUnion<1>
+
+type TestUnion<A, B = A> = A  extends A ? { a: A, b: B} : never;
+type res109 = TestUnion<'a' | 'b'>
+
+/* 数组转联合 */
+type arrToUnion<T extends Array<unknown>> = T[number] 
+type res110 = arrToUnion<[1, 2, 3]>
+
+/* BEM命名 */
+type bem<block extends string, ele extends string[], modifier extends string[]> = `${block}__${ele[number]}--${modifier[number]}`
+type res111 = bem<'zone', ['title', 'content'], ['isEditing', 'isSelected']>
+
+/* 两两组合 */
+type combine<T extends string, P extends string> = `${T | P}` | `${T}${P}` | `${P}${T}`
+type res112 = combine<'a', 'b'>
+
+/* 全组合 */
+type allCombine<T extends string, P extends string = T> =
+  T extends T 
+  ? combine<T, allCombine< Exclude<P, T> >>
+  : never
+type res113 = allCombine<'a' | 'b' | 'c'>
+ 
+/* 排除 */
+// 若A是B的子类型，则返回never; 若不是，则返回A
+// 利用union分布式的特性，可以排除union中的某些类型
+// 即 排除A联合类型中的所有B类型（所有受B类型约束的子类型）
+type exclude<A, B> = A extends B ? never : A
+type res114 = exclude<'a', string>
+type res115 = exclude<'a', 'a' | 'b'>
+type res116 = exclude<'a' | 'b', 'a'>
+type res117 = exclude<'a' | 'b' | 1, string>
+type res118 = exclude<'a' | 'b' | 'c', 'a' | 'b'>
+
+
+
